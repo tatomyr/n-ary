d330bb1c38466dcd41aba13c866f03cc44db1cd0
tatomyr <andrew.tatomyr@gmail.com>
2017-06-06 22:08:04 +0300

    BREAKING SYNTAX CHANGE

--- n-ary.js
+++ n-ary.js
@@ -1,31 +1,31 @@
-const nAry = (base = 2) => {
+const nAry = (base = 2) => (length = 1) => {
   // Max each digit value:
   const range = base - 1;
 
   // Zero vector:
-  const zero = length => Array(length).fill(0);
+  const zero = () => Array(length).fill(0);
 
   // The last vector:
-  const last = length => Array(length).fill(range);
+  const last = () => Array(length).fill(range);
 
   // A random vector:
-  const random = length => zero(length).map(digit => Math.floor(Math.random() * base));
+  const random = () => zero().map(digit => Math.floor(Math.random() * base));
 
   // (base)-ary incrementation:
-  const inc = vector => {
+  const inc = (vector) => {
     const index = vector.findIndex(digit => digit < range);
     return index === -1 ? (
-      zero(vector.length)
+      zero()
     ) : [
-      ...zero(index),
+      ...Array(index).fill(0),
       vector[index] + 1,
       ...vector.filter((_, i) => i > index)
     ];
   }
 
   // Brute force:
-  const all = length => {
-    const vectors = [zero(length)];
+  const all = () => {
+    const vectors = [zero()];
     for (let i = 1; i < Math.pow(base, length); i++) {
       vectors.push(inc(vectors[i - 1]));
     }
@@ -44,10 +44,10 @@ const nAry = (base = 2) => {
   //   }
   //   return vectors;
   // }
-  const verge = length => all(length).filter(vector => vector.some(digit => digit === range));
+  const verge = () => all().filter(vector => vector.some(digit => digit === range));
 
   // Set of (count) random vectors:
-  const randomSet = length => count => zero(count).map(() => random(length));
+  const randomSet = count => Array(count).fill(0).map(() => random());
 
   // Convert (base)-ary number to decimal:
   const index = vector => vector.reduce((sum, item, i) => sum + item * Math.pow(base, i), 0);
@@ -55,22 +55,11 @@ const nAry = (base = 2) => {
   const sumOfDigits = vector => vector.reduce((sum, item) => sum + +item, 0);
 
   // Returns all vectors with specified sum of digits value
-  const sumEquals = length => value => all(length).filter(vector => sumOfDigits(vector) === value);
+  const sumEquals = value => all().filter(vector => sumOfDigits(vector) === value);
 
   // Generates chunks of datasets.
   // const chunk = length => maxChunkCount => ...
 
-  /*
-  // Length subfunction:
-  const length = length => {
-    const sumEquals = value => all(length).filter(vector => sumOfDigits(vector) === value);
-
-    return {
-      randomSet,
-      sumEquals,
-    };
-  }
-  */
 
   /*
     Or, maybe, we should type smth like: `nAry(base)(length || vector).inc()`
